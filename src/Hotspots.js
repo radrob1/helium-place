de666c9a46c2e50a25f0b1c9f23747a459b0e76b
Tarek Ali <tarekali15@outlook.com>
2021-02-01 09:03:30 -0600

    formatting of files and gitignored eslintcache

--- src/Hotspots.js
+++ src/Hotspots.js
@@ -1,27 +1,27 @@
-import * as React from 'react';
-import { PureComponent } from 'react';
-import { Popup } from 'react-map-gl';
+import * as React from "react";
+import { PureComponent } from "react";
+import { Popup } from "react-map-gl";
 import HotspotInfo from "./HotspotInfo";
-
-
 
 // Important for perf: the markers never change, avoid rerender when the map viewport changes
 export default class Hotspots extends PureComponent {
   onClick(hotspot) {
     console.log("marker click: ", hotspot);
-    this.setState({popupname: hotspot})
+    this.setState({ popupname: hotspot });
   }
 
   render() {
     const { data } = this.props;
     const state = {
-      popupname: null
-    }
+      popupname: null,
+    };
     console.log(data);
-    return data.map(
-      (hotspot, index) =>
-      <div className={hotspot.properties.name} onClick={this.onClick(hotspot.properties.name)}>
-        {this.state.popupname  != null  && (
+    return data.map((hotspot, index) => (
+      <div
+        className={hotspot.properties.name}
+        onClick={this.onClick(hotspot.properties.name)}
+      >
+        {this.state.popupname != null && (
           <Popup
             tipSize={8}
             anchor="top"
@@ -33,7 +33,7 @@ export default class Hotspots extends PureComponent {
             <HotspotInfo info={hotspot} />
           </Popup>
         )}
-        </div>
-    );
+      </div>
+    ));
   }
 }
