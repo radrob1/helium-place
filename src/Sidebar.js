fc490231cacfcf59d3ce0412231c96d3f6fae66b
radrob1 <rad.cryptotrader@gmail.com>
2021-04-26 14:59:46 -0400

    a-ads

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -220,7 +220,9 @@ export default class Sidebar extends React.Component {
                         <p className="description">Try a Nebra Helium Miner</p>
                       </div>
                     </a>
-
+                    <div className="ad">
+                      <iframe data-aa="1620515" src="//ad.a-ads.com/1620515?size=200x200" scrolling="no" style={{width:"200px", height:"200px", border:"0px", padding:0, overflow:"hidden"}} allowtransparency="true"></iframe>
+                    </div>
                   </div>
                 </div>
               </div>
