e7619b66678ce325281322d8b3cede226d143f58
radrob1 <rad.cryptotrader@gmail.com>
2021-08-29 15:21:42 -0400

    hotspotty link

--- src/components/Sidebar/SidebarControls/SidebarControls.js
+++ src/components/Sidebar/SidebarControls/SidebarControls.js
@@ -260,6 +260,12 @@ export const SidebarControls = ({
               <p className="description">Try an upgraded antenna from RAK Wireless</p>
             </div>
           </a>
+          <a target="_blank" rel="noreferrer" href='https://hotspotty.net' className="ad">
+            <div className="ad-content">
+              <h4 className="title">Hotspotty</h4>
+              <p className="description">Try this awesome hotspot planning tool!</p>
+            </div>
+          </a>
           <div className="ad">
             <iframe data-aa="1620569" src="//ad.a-ads.com/1620569?size=468x60" scrolling="no" style={{ width: "468px", height: "60px", border: "0px", padding: 0, overflow: "hidden" }} allowtransparency="true"></iframe>               
           </div>
