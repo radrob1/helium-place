c1d5ae35b9e8f14a5667a2340f7986ed2ef9ba0b
radrob1 <rad.cryptotrader@gmail.com>
2021-09-20 08:53:35 -0400

    github link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => {
     <div className="App">
       I have decided to retire Helium.place, but don't worry you can use the new Hotspotty app for all your hotspot placement needs!
       <p></p><div><a href="https://www.hotspotty.net">Hotspotty 2.0</a></div>
+      <p></p><div>If you would like to run helium.place yourself, you can find all the code here: <a href="https://github.com/radrob1/helium-place">https://github.com/radrob1/helium-place</a></div>
     </div>
   );
 };
